32c32702447c226994b467d1826832f8d9ff0cf1
Dklait_Gaming <m.ivancich.sn@gmail.com>
2025-04-10 16:02:29 +0200

    Correction de l'importation du composant DashboardAdmin dans App.jsx

--- front/vite-project/src/App.jsx
+++ front/vite-project/src/App.jsx
@@ -22,7 +22,7 @@ import VieQuotidienne from './pages/vieQuotidienne';
 //importation des fichiers .jsx
 import Home from './pages/home.jsx';
 import AuthentificationAdmin from './pages/authentificationAdmin.jsx';
-import AdminDashboard from './pages/AdminDashboard.jsx';
+import DashboardAdmin from './pages/dashboardAdmin.jsx';
 
 function App() {
   return (
@@ -44,7 +44,7 @@ function App() {
           <Route path='/vie-quotidienne' element={<VieQuotidienne />} />
           <Route path='/admission' element={<Admission />} />
           <Route path='/authentification-admin' element={<AuthentificationAdmin/>} />
-          <Route path='/dashboard-admin' element={<AdminDashboard/>} />
+          <Route path='/dashboard-admin' element={<DashboardAdmin/>} />
           <Route path="/" element={<Navigate to="/accueil" />} />
         </Routes>
       </Router>
