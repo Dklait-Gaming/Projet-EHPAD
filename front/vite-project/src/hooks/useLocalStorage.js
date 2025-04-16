68a516c615e421e923135fc600c1ea4dc4178bcf
Dklait_Gaming <m.ivancich.sn@gmail.com>
2025-04-16 12:03:01 +0200

    ajout de commentaires pour clarifier l'importation et la déclaration de la fonction useLocalStorage

--- front/vite-project/src/hooks/useLocalStorage.js
+++ front/vite-project/src/hooks/useLocalStorage.js
@@ -1,5 +1,7 @@
+//importation des différents hooks et composants nécessaires
 import { useState, useEffect } from "react";
 
+//déclaration de ma fonction par défaut useLocalStorage
 export function useLocalStorage(key, initialValue) {
   const [value, setValue] = useState(() => {
     try {
